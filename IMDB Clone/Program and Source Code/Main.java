b1e6f8cccbc96eac20a9aab787d2bf77bc23b085
godfreybuddy <48738875+godfreybuddy@users.noreply.github.com>
2019-07-30 16:15:39 -0400

    Add files via upload

---
+++ IMDB Clone/Program and Source Code/Main.java
@@ -0,0 +1,29 @@
+import Backend.SQLServer;
+import UI.Window;
+import mdlaf.*;
+import java.sql.SQLException;
+import javax.swing.*;
+
+/**
+ * 474 SQL Server interacting program
+ * By Maksim Samoylov, Tyree Mitchell, Phillip Zubov, Buddy Godfrey
+ * Group 44
+ */
+public class Main {
+
+    /**
+     * Main method to just launch the program
+     */
+    public static void main( String[] args ) throws SQLException {
+        try {
+            UIManager.setLookAndFeel(new MaterialLookAndFeel());
+        } catch(Exception ignored){}
+
+        SQLServer server = new SQLServer();
+        Window window = new Window(server);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800,600);
+        window.setVisible(true);
+        server.connectTo("71.63.48.66", 3306, "imdb", "program", "Yeehaw420$");
+    }
+}
