69c271936378b939fb6a31128be22fc09483a2c3
godfreybuddy <48738875+godfreybuddy@users.noreply.github.com>
2019-07-30 16:42:26 -0400

    Add files via upload

---
+++ TextEx/TextEx.java
@@ -0,0 +1,26 @@
+package textex;
+
+import ui.StartScreen;
+
+/**
+ * Initialize and assign all GUI components to a JFrame.
+ * 
+ * @author Phillip Zubov
+ * @author Tyree Mitchell
+ * @author Anthony Thellaeche
+ *
+ */
+public class TextEx
+{
+
+  /**
+   * Initialize all GUI components.
+   * 
+   * @param args
+   *          unused
+   */
+  public static void main(String[] args)
+  {
+    new StartScreen();
+  }
+}
